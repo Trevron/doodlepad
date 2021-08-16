dc904d7e62cae40e6a18baadd095de9bd62c9a7e
Trevron <trevormakesmusic@gmail.com>
2021-08-16 01:48:28 -0700

    -Added button for canvas resolution. -Added function to create new canvas based on user input. -Fixed some style options.

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
+let canvasWidth = 0;
+let canvasHeight = 0;
+
+const newButton = document.getElementById('newCanvas');
+
 const canvas = document.querySelector('.canvas');
 
 
-for (let i = 0; i < 16; i++){
-    const gridSquare = document.createElement('div');
-    gridSquare.classList.add('gridSquare');
-    canvas.appendChild(gridSquare);
+
+
+function createNewCanvas() {
+    canvasWidth = prompt('Enter canvas width:', 32);
+    canvasHeight = prompt('Enter canvas height:', 32);
+
+    // Add grid rules to css style
+    canvas.setAttribute('style', 'grid-template-columns: repeat(' 
+            + canvasWidth + ', 16px); grid-template-rows: repeat(' 
+            + canvasHeight + ', 16px);');
+    
+    // Populate divs
+    for (let i = 0; i < canvasWidth * canvasHeight; i++){
+        const gridSquare = document.createElement('div');
+        gridSquare.classList.add('gridSquare');
+        canvas.appendChild(gridSquare);
+    }
 }
+
+newButton.addEventListener('click', createNewCanvas);
