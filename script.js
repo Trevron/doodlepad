72b49129605cbb981d787f2171c65d4c9af40547
Trevron <trevormakesmusic@gmail.com>
2021-08-18 10:06:07 -0700

    -Fixed drawing functionality to allow single clicks as well as strokes. -Added additional buttons for brush, eraser, and fill tools. -Added eraser and brush functionality. -Updated some style options.

--- script.js
+++ script.js
@@ -3,9 +3,15 @@ let canvasHeight = 0;
 let isCanvas = false;
 let currentColor = 'black';
 let isClicking = false;
+let drawing = true;
+let erasing = false;
+let filling = false;
 
 const newButton = document.getElementById('newCanvas');
 const colorButton = document.getElementById('colorButton')
+const brushButton = document.getElementById('brushTool');
+const fillButton = document.getElementById('fillTool');
+const eraserButton = document.getElementById('eraserTool');
 const colorDiv = document.getElementById('currentColor');
 const canvas = document.querySelector('.canvas');
 
@@ -31,10 +37,10 @@ function createNewCanvas() {
         canvas.appendChild(gridSquare);
     }
 
-
     newButton.textContent='Reload Page';
     isCanvas = true;
 }
+
 
 function changeColor() {
     currentColor = prompt('Enter a color name', currentColor);
@@ -43,8 +49,14 @@ function changeColor() {
 }
 
 function drawPixel(pixelTarget) {
-    pixelTarget.setAttribute('style', 'background-color: ' 
+    if (erasing) {
+        pixelTarget.setAttribute('style', 'background-color: white;');
+    }
+    if (drawing) {
+        pixelTarget.setAttribute('style', 'background-color: ' 
             + currentColor + ';' );
+    }
+    
 }
 
 canvas.addEventListener('mousedown', (e) => {
@@ -61,7 +73,48 @@ canvas.addEventListener('mouseover', function (e) {
     }
 });
 
-
+function toolSelect(tool) {
+    switch (tool) {
+        case 'brush':
+            drawing = true;
+            erasing = false;
+            filling = false;
+            brushButton.classList.add('selected');
+            eraserButton.classList.remove('selected');
+            fillButton.classList.remove('selected');
+            console.log('Brush selected');
+            break;
+        case 'eraser':
+            drawing = false;
+            erasing = true;
+            filling = false;
+            eraserButton.classList.add('selected');
+            brushButton.classList.remove('selected');
+            fillButton.classList.remove('selected');
+            console.log('Eraser selected');
+            break;
+        case 'fill':
+            drawing = false;
+            erasing = false;
+            filling = true;
+            fillButton.classList.toggle('selected');
+            brushButton.classList.remove('selected');
+            eraserButton.classList.remove('selected');
+            console.log('Fill selected');
+            break;
+        default:
+            alert('There was an error!');
+    }
+}
 
 newButton.addEventListener('click', createNewCanvas);
 colorButton.addEventListener('click', changeColor);
+brushButton.addEventListener('click', function () {
+    toolSelect('brush');
+});
+eraserButton.addEventListener('click', function () {
+    toolSelect('eraser');
+});
+fillButton.addEventListener('click', function () {
+    toolSelect('fill');
+});
